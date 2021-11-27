38520da2ef3054b2d0d4e2f3431033b9ba13ff3c
elvinyeka <elvinyeka@gmail.com>
2021-11-27 17:59:16 +0400

    themecontext, themeprovider, storage, useTheme

---
+++ src/model/ChangeCssRootsVariables.ts
@@ -0,0 +1,16 @@
+import { Theme } from "../context/ThemeContext";
+
+export const ChangeCssRootVariables = (theme: Theme) => {
+  const root = document.querySelector(':root') as HTMLElement;
+
+  const components = [
+    'body-background',
+    'components-background',
+    'card-background',
+    'card-shadow',
+    'text-color']
+
+  components.forEach(component => {
+    root.style.setProperty(`--${component}-default`, `var(--${component}-${theme})`);
+  })
+}
--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 
 import GlobalSvgSelector from '../../assets/icons/global/GlobalSvgSelector';
+import { Theme } from '../../context/ThemeContext';
+import { useTheme } from '../../hooks/useTheme';
 import s from './Header.module.scss';
 
 interface Props {
@@ -9,6 +11,7 @@ interface Props {
 }
 
 const Header = (props: Props) => {
+  const theme = useTheme();
   const options = [
     { value: 'choose', label: 'Şəhər seçin' },
     { value: 'baku', label: 'Baku' },
@@ -20,7 +23,7 @@ const Header = (props: Props) => {
   const colorStyles = {
     control: (styles: any) => ({
       ...styles,
-      backgroundColor: 0 ? '#4f4f4f' : 'rgba(71,147,255,0.2)',
+      backgroundColor: theme.theme === Theme.DARK ? '#4f4f4f' : 'rgba(71,147,255,0.2)',
       width: '194px',
       height: '37px',
       border: 'none',
@@ -29,10 +32,16 @@ const Header = (props: Props) => {
     }),
     singleValue: (styles: any) => ({
       ...styles,
-      color: 0 ? '#fff' : '#000',
+      color: theme.theme === Theme.DARK ? '#fff' : '#000',
 
     }),
   }
+
+  const changeTheme = () => {
+
+    theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  }
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
@@ -42,7 +51,9 @@ const Header = (props: Props) => {
         <div className={s.title}>Sunny Weather</div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change_theme}>
+        <div
+          onClick={changeTheme}
+          className={s.change_theme}>
           <GlobalSvgSelector id='change-theme' />
         </div>
         <Select defaultValue={options[0]} styles={colorStyles} options={options} />
